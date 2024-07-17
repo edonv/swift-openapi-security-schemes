3fc216c05180344399884608560308bb0f6cd5d2
Edon <edon@valdman.works>
2024-07-17 12:51:16 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "swift-openapi-security-schemes",
-    platforms: [.macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .visionOS(.v1)],
+    platforms: [
+        .macOS(.v11),
+        .iOS(.v14),
+        .tvOS(.v14),
+        .watchOS(.v7),
+        .visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
