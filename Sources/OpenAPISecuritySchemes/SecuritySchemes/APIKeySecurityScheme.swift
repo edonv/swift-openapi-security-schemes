e79cb7e0a11d328f8e81f5147625d6b63446941e
Edon <edon@valdman.works>
2024-07-17 17:29:33 -0500

    Created subprotocols for API key and HTTP schemes

---
+++ Sources/OpenAPISecuritySchemes/SecuritySchemes/APIKeySecurityScheme.swift
@@ -0,0 +1,31 @@
+//
+//  APIKeySecurityScheme.swift
+//  
+//
+//  Created by Edon Valdman on 7/17/24.
+//
+
+import Foundation
+
+public protocol APIKeySecurityScheme: SecurityScheme {
+    /// The name of the header, query or cookie parameter to be used.
+    static var name: String { get }
+    
+    /// The location of the API key.
+    ///
+    /// Valid values are "query", "header" or "cookie".
+    static var `in`: APIKeySecuritySchemeLocation { get }
+    
+    /// The API key.
+    var key: String { get }
+}
+
+extension APIKeySecurityScheme {
+    public static var type: SecuritySchemeType { .apiKey }
+}
+
+public enum APIKeySecuritySchemeLocation: String, Hashable, Sendable, Codable {
+    case query
+    case header
+    case cookie
+}
---
+++ Sources/OpenAPISecuritySchemes/SecuritySchemes/HTTPSecurityScheme.swift
@@ -0,0 +1,69 @@
+//
+//  HTTPSecurityScheme.swift
+//
+//
+//  Created by Edon Valdman on 7/17/24.
+//
+
+import Foundation
+import HTTPTypes
+
+public protocol HTTPSecurityScheme: SecurityScheme {
+    /// The name of the HTTP Authorization scheme to be used in the `Authorization` header as defined in [[RFC7235](https://spec.openapis.org/oas/latest.html#bib-RFC7235)].
+    ///
+    /// The values used SHOULD be registered in the [IANA Authentication Scheme registry](https://www.iana.org/assignments/http-authschemes/http-authschemes.xhtml).
+    static var scheme: HTTPSecuritySchemeName { get }
+    
+    func mutateRequest(_ request: inout HTTPRequest, body: inout HTTPBody?)
+}
+
+extension HTTPSecurityScheme {
+    public static var type: SecuritySchemeType { .http }
+    public static var headerName: String { "Authorization" }
+}
+
+// MARK: - BasicHTTPSecurityScheme
+
+public protocol BasicHTTPSecurityScheme: HTTPSecurityScheme {
+    var username: String { get }
+    var password: String { get }
+}
+
+extension BasicHTTPSecurityScheme {
+    public static var scheme: HTTPSecuritySchemeName { .basic }
+    
+    private var _credentials: String {
+        "\(username):\(password)"
+            .data(using: .utf8)!
+            .base64EncodedString()
+    }
+    
+    public func mutateRequest(_ request: inout HTTPRequest, body: inout HTTPBody) {
+        request.headerFields[.authorization] = "Basic \(_credentials)"
+    }
+}
+
+// MARK: - BearerHTTPSecurityScheme
+
+public protocol BearerHTTPSecurityScheme: HTTPSecurityScheme {
+    static var format: HTTPSecuritySchemeName.BearerFormat? { get }
+    var accessToken: String { get }
+}
+
+extension BearerHTTPSecurityScheme {
+    public static var scheme: HTTPSecuritySchemeName { .bearer(format) }
+    
+    public func mutateRequest(_ request: inout HTTPRequest, body: inout HTTPBody) {
+        request.headerFields[.authorization] = "Bearer \(accessToken)"
+    }
+}
+
+public enum HTTPSecuritySchemeName: Hashable, Sendable, Codable {
+    case basic
+    
+    case bearer(BearerFormat?)
+    
+    public enum BearerFormat: String, Hashable, Sendable, Codable {
+        case jwt = "JWT"
+    }
+}
