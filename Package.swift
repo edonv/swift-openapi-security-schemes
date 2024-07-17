ffd0602d6a86ae257f42c8eb72e9cd57982aeadb
Edon <edon@valdman.works>
2024-07-17 12:51:03 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,15 +9,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "swift-openapi-security-schemes",
-            targets: ["swift-openapi-security-schemes"]),
+            targets: ["swift-openapi-security-schemes"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-openapi-security-schemes"),
+            name: "swift-openapi-security-schemes"
+        ),
         .testTarget(
             name: "swift-openapi-security-schemesTests",
-            dependencies: ["swift-openapi-security-schemes"]),
+            dependencies: ["swift-openapi-security-schemes"]
+        ),
     ]
 )
