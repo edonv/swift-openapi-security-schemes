2574de304ced9a128516e207cf9d95fdf4670734
Edon <edon@valdman.works>
2024-07-18 12:10:48 -0500

    Update SecurityScheme.swift
    Added a new required function that defines how a `SecurityScheme` applies itself to an operation.

--- Sources/OpenAPISecuritySchemes/SecurityScheme.swift
+++ Sources/OpenAPISecuritySchemes/SecurityScheme.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+import OpenAPIRuntime
+import HTTPTypes
+
 // MARK: - SecurityScheme
 
 /// Don't conform to this protocol directly. Instead, use one of the sub-protocols, depending on the ``type``.
@@ -17,6 +20,17 @@ public protocol SecurityScheme {
     ///
     /// Valid values are "apiKey", "http", "mutualTLS", "oauth2", "openIdConnect".
     static var type: SecuritySchemeType { get }
+    
+    /// Applies this `SecurityScheme` to an operation.
+    /// - Parameters:
+    ///   - operationID: The identifier of the OpenAPI operation.
+    ///   - request: The HTTP request to modify.
+    ///   - body: The HTTP request body to modify.
+    func applyScheme(
+        toOperation operationId: String,
+        request: inout HTTPRequest,
+        body: inout HTTPBody?
+    ) async throws
 }
 
 public enum SecuritySchemeType: String, Hashable, Sendable, Codable {
