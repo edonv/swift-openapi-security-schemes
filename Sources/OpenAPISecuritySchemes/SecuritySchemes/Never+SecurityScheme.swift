85f0a129edfa8069915e96642c281b1548653e45
Edon <edon@valdman.works>
2024-07-18 12:06:15 -0500

    Create Never+SecurityScheme.swift
    Added `SecurityScheme` conformance to `Never`, in case `SecuritySchemeMiddleware` doesn't use `scheme`.

---
+++ Sources/OpenAPISecuritySchemes/SecuritySchemes/Never+SecurityScheme.swift
@@ -0,0 +1,20 @@
+//
+//  Never+SecurityScheme.swift
+//
+//
+//  Created by Edon Valdman on 7/18/24.
+//
+
+import Foundation
+import OpenAPIRuntime
+import HTTPTypes
+
+extension Never: SecurityScheme {
+    public static let type: SecuritySchemeType = .apiKey
+    
+    public func applyScheme(
+        toOperation operationId: String,
+        request: inout HTTPTypes.HTTPRequest,
+        body: inout OpenAPIRuntime.HTTPBody?
+    ) async throws {}
+}
