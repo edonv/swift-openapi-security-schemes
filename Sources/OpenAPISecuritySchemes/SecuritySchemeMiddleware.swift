2b1339b27f7718b4a9dffd4c2eb80b359f1121ab
Edon <edon@valdman.works>
2024-07-18 12:25:39 -0500

    Update SecuritySchemeMiddleware.swift
    Created a new delegate protocol for `SecuritySchemeMiddleware`. This allows for any `SecurityScheme` to be used for any operation.

--- Sources/OpenAPISecuritySchemes/SecuritySchemeMiddleware.swift
+++ Sources/OpenAPISecuritySchemes/SecuritySchemeMiddleware.swift
@@ -19,3 +19,23 @@ public protocol SecuritySchemeMiddleware: ClientMiddleware {
 extension SecuritySchemeMiddleware where Scheme == Never {
     public var scheme: Never? { nil }
 }
+
+// MARK: - SecuritySchemeMiddlewareDelegate
+
+public protocol SecuritySchemeMiddlewareDelegate: AnyObject {
+    /// Asks the delegate what security scheme to use for any given operation.
+    ///
+    /// Because the return type uses `any`, you can use this function to return different schemes for different operations.
+    ///
+    /// This can be used in conjunction with ``SecuritySchemeMiddleware/scheme-21xl3``. This function's return value takes precendence over ``SecuritySchemeMiddleware/scheme-21xl3``, unless this returns `nil`.
+    ///
+    /// If `nil` is returned, no scheme will be applied.
+    /// - Parameters:
+    ///   - middleware: The ``SecuritySchemeMiddleware`` that this delegate belongs to..
+    ///   - operationId: The identifier of the OpenAPI operation in question.
+    /// - Returns: A ``SecurityScheme`` to apply to this operation, or `nil` to not apply any scheme.
+    func securityScheme(
+        _ middleware: any SecuritySchemeMiddleware,
+        forOperation operationId: String
+    ) -> (any SecurityScheme)?
+}
