33672cf05ae0e60e3209d03ab37f6adaae35a0a8
Edon <edon@valdman.works>
2024-09-15 13:37:08 -0500

    Update SecurityError.swift
    Refactored error type.

--- Sources/OpenAPISecuritySchemes/SecurityError.swift
+++ Sources/OpenAPISecuritySchemes/SecurityError.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import HTTPTypes
 
-public enum SecurityError: Error, Hashable, Sendable {
-    case invalidSecurity(operationID: String)
+public struct SecuritySchemeError<S: SecurityScheme>: Error, Hashable, Sendable {
+    public let scheme: SecuritySchemeType
+    
+    public let operationID: String
+    
+    public let request: HTTPRequest
+    
+    init(
+        scheme: S.Type = S.self,
+        operationID: String,
+        request: HTTPRequest
+    ) {
+        self.scheme = S.type
+        self.operationID = operationID
+        self.request = request
+    }
 }
